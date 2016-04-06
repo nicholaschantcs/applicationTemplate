8cc84156a861035575ce30567971d361dcfe2380
Nicholas <nicholas.chan@thecitysecret.com>
2016-04-06 12:07:19 +0100

    refined frontend to include encryption for password handling registration now creates a new row in database

--- site/public/javascripts/mainController.js
+++ site/public/javascripts/mainController.js
@@ -6,16 +6,20 @@ angular
   .service('appPubSub', seneca.ng.pubsub())
   .controller('mainCtrl', function($scope, $http, appAPI, appPubSub) {
     $scope.greetings = "Welcome";
+	
     $scope.$watch('nameInput', function(newV, oldV) {
       if (newV != undefined)
         $scope.greetings = "Welcome " + newV;
     })
+	
     $http.get('/auth/user').then(function(callback) {
       $scope.user = callback.data.user;
-      if ($scope.user)
+      if ($scope.user) {
         $scope.greetings = "Welcome " + $scope.user.nick;
-		$scope.nameInput = $scope.user.nick;
+        $scope.nameInput = $scope.user.nick;
+      }
     })
+	
     $scope.greetUser = function() {
       $http.post('/greetUsers', {
         user: $scope.nameInput
@@ -26,9 +30,44 @@ angular
         console.log(data);
       })
     }
+	
     $scope.generateRandomNumber = function() {
       appAPI.get('number/displayRandomNumber', function(data) {
         $scope.randomNumber = data.data;
+      })
+    }
+	
+
+
+    $scope.register = function() {
+	   var encrpytedText = CryptoJS.AES.encrypt($scope.registerPassword, "TheC!tySecret");
+	   var hashedPassword = encrpytedText.toString();
+		
+      var newUser = {
+        username: $scope.registerUsername,
+        password: $scope.registerPassword,
+      }
+      $http.post('auth/register',newUser)
+        .then(function(success) {
+		  newUser.password = hashedPassword;
+          $http.post('/users/register', {user:newUser}).then(function(callback) {
+			window.location="/";
+		  })
+        })
+    }
+
+    $scope.login = function() {
+      $http.post("auth/login", {
+        username: $scope.username,
+        password: $scope.password
+      }).then(function(success, err) {
+        $http.get('/auth/user').then(function(callback) {
+          $scope.user = callback.data.user;
+          if ($scope.user) {
+            $scope.greetings = "Welcome " + $scope.user.nick;
+            $scope.nameInput = $scope.user.nick;
+          }
+        })
       })
     }
   })
